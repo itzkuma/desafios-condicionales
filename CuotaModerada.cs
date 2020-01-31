6d9dce253afb208fdd78b229e497d551b4e83839
itzkuma <60122679+itzkuma@users.noreply.github.com>
2020-01-30 21:14:22 -0500

    Add files via upload
    Por David Martínez Cárdenas

---
+++ CuotaModerada.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoCuotaModeradora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Copie su salario");
+            double salario = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Su tarifa a pagar es:");
+
+            if (salario < (877803 + 877803))
+            {
+
+                Console.WriteLine("Tarifa A");
+            }
+            else if  ((877803 * 2) <= salario && salario < (877803 * 5))
+            {
+                Console.WriteLine("Tarifa B");
+            }
+            else if (salario >= (877803 * 5))
+            {
+
+                Console.WriteLine("Tarifa C");
+            }
+        }
+    }
+}
+    
+
